12f85a80c4163442b7989779e1b178e316fac9ea
spirit-124 <bajrangofficial580@gmail.com>
2024-01-21 15:53:50 +0530

    Database Connected

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database Connected!!");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 const app = express();
 
