3d4cdf911e28135d170537f480b72134e403887a
spirit-124 <bajrangofficial580@gmail.com>
2024-02-02 11:49:13 +0530

    [Completed] Profile Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { SignIn, About, Profile, Home, SignUp } from "./page";
-import { Header } from "./components";
+import { Header, PrivateRoute } from "./components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
@@ -10,7 +10,10 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+
+        <Route path="/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -1,5 +1,50 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className=" p-3 max-w-lg mx-auto">
+      {" "}
+      <h1 className=" font-semibold text-center text-3xl my-7">Profile</h1>
+      <form className=" flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full cursor-pointer  h-24 w-24 object-cover self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id=""
+          className="border p-3 rounded-lg"
+        />
+        <button className=" bg-slate-700 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 text-white p-3">
+          {" "}
+          update
+        </button>
+      </form>
+      <div className=" mt-4 flex  justify-between">
+        <span className=" text-red-700 cursor-pointe font-semibold">
+          Delete Account
+        </span>
+        <span className=" text-red-700 cursor-pointe font-semibold">
+          Sign Out
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
