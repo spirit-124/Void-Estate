83dceeaaab5060d0456f65591b6db80a2120365f
spirit-124 <bajrangofficial580@gmail.com>
2024-02-03 20:04:27 +0530

    Minor Changes

--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -1,13 +1,38 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { getStorage } from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+
+  useEffect(() => {
+    if (file) {
+      handleFileupload(file);
+    }
+  }, [file]);
+
+  const handleFileupload = (file) => {
+    const storage = getStorage(app);
+  };
   return (
     <div className=" p-3 max-w-lg mx-auto">
       {" "}
       <h1 className=" font-semibold text-center text-3xl my-7">Profile</h1>
       <form className=" flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          hidden
+        />
         <img
+          onClick={() => {
+            fileRef.current.click();
+          }}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full cursor-pointer  h-24 w-24 object-cover self-center mt-2"
